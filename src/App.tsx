7d484c94b5c79fa6ebd31e66bcfe7e41eaa7d858
20chan <2@0chan.dev>
2021-04-03 08:42:49 +0000

    project list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Container, Dropdown, Header, Icon, Label, Segment } from 'semantic-ui-react';
+import { Container, Dropdown, Header, Icon, Label, List, Segment } from 'semantic-ui-react';
 import { priorities, Tag, projects } from './data';
 
 function App() {
   const allTags = Object.values(Tag);
   const [tagsSelected, setTagsSelected] = useState<string[]>(allTags);
   const tagsNotSelected = allTags.filter(x => !tagsSelected.includes(x));
+
+  const selectProjects = projects.filter(x => x.tags.find(y => tagsSelected.includes(y)));
 
   const handleDeleteTag = (tag: string) => {
     const tags = tagsSelected.filter(x => x !== tag);
@@ -26,7 +28,7 @@ function App() {
         <Segment attached>
           {
             tagsSelected.map(x => <>
-              <Label color='blue'>
+              <Label color='blue' key={x}>
                 {x}
                 <Icon name='delete' link onClick={() => handleDeleteTag(x)} />
               </Label>
@@ -34,12 +36,32 @@ function App() {
           }
           {
             tagsNotSelected.map(x => <>
-              <Label>
+              <Label key={x}>
                 {x}
                 <Icon name='plus' link onClick={() => handleAddTag(x)} />
               </Label>
             </>)
           }
+        </Segment>
+
+        <Header content={`projects ${selectProjects.length}/${projects.length}`} textAlign='center' attached='top' block />
+        <Segment attached>
+          <List>
+            {
+              selectProjects.map(x => <>
+                <List.Item>
+                  <List.Content>
+                    <List.Header as='a'>
+                      {x.name}
+                    </List.Header>
+                    <List.Description>
+                      {x.description}
+                    </List.Description>
+                  </List.Content>
+                </List.Item>
+              </>)
+            }
+          </List>
         </Segment>
       </Container>
     </div>
